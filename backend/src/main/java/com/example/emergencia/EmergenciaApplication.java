4b9b861d87ddd8d4fa959cc0c7ea37449bf9926e
jgutierrez10 <jgutierrezreyes2003@gmail.com>
2026-05-11 12:30:13 -0400

    frontend

---
+++ backend/src/main/java/com/example/emergencia/EmergenciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.emergencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergenciaApplication.class, args);
+	}
+
+}
