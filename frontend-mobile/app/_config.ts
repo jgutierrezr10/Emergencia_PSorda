52cd99c5dc1f4c905a2a5296abee9fac17da4f87
jgutierrezreyes2003 <jgutierrezreyes2003@gmail.com>
2026-06-30 16:46:49 -0400

    fix login web

--- frontend-mobile/app/_config.ts
+++ frontend-mobile/app/_config.ts
@@ -2,24 +2,7 @@ import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
 const getBaseUrl = () => {
-  const productionUrl = process.env.EXPO_PUBLIC_BACKEND_URL;
-
-  if (productionUrl) {
-    return productionUrl;
-  }
-
-  if (Platform.OS === 'web') return 'http://localhost:8080';
-
-  const debuggerHost = Constants.expoConfig?.hostUri || Constants.manifest?.debuggerHost;
-
-  if (debuggerHost) {
-    const ipAutomatica = debuggerHost.split(':').shift();
-    return `http://${ipAutomatica}:8080`;
-  }
-
-  const ipRespaldo = process.env.EXPO_PUBLIC_BACKEND_IP || 'localhost';
-
-  return `http://${ipRespaldo}:8080`;
+  return 'https://emergenciapsorda-production.up.railway.app';
 };
 
 export const baseUrl = getBaseUrl();
