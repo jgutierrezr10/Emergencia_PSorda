64a91e5d9320e95cfc025be68368eeff8e2dedb4
jgutierrezreyes2003 <jgutierrezreyes2003@gmail.com>
2026-07-01 00:30:14 -0400

    Fix data.sql to remove comisarias

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,14 +3,8 @@ INSERT INTO usuarios (nombre, apellido, telefono, rut, clave, estado, rol) VALUE
 ('Maria', 'Gomez', '912345678', '67676767-6', '$2a$10$RR6E18EqG38seiGsJQPqeOTpRIaPNb0Vs40n/OvewLclsUErB6YyO', 'Activo', 'Sordo') 
 ON CONFLICT (rut) DO UPDATE SET rol = EXCLUDED.rol;
 
-INSERT INTO comisarias (nombre, direccion)
-SELECT '1ra Comisaría de Santiago', 'Santo Domingo 123'
-WHERE NOT EXISTS (
-    SELECT 1 FROM comisarias WHERE nombre = '1ra Comisaría de Santiago'
-);
-
-INSERT INTO carabineros (nombre, numero, rango, comisaria_id, usuario_id)
-SELECT 'Juan Perez', 133, 'Cabo', (SELECT id FROM comisarias WHERE nombre = '1ra Comisaría de Santiago'), id FROM usuarios WHERE rut = '12345678-9'
+INSERT INTO carabineros (nombre, numero, rango, usuario_id)
+SELECT 'Juan Perez', 133, 'Cabo', id FROM usuarios WHERE rut = '12345678-9'
 AND NOT EXISTS (
     SELECT 1 FROM carabineros 
     WHERE usuario_id = (SELECT id FROM usuarios WHERE rut = '12345678-9')
